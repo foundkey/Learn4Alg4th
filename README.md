c66f8004d1367b3392503e49a6f60a0762425c6e
foundkey <469456770@qq.com>
2019-04-18 14:58:07 +0800

    Import algs4.jar

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Learn4Alg4th
+Book url: https://algs4.cs.princeton.edu/home/
+Github: https://github.com/kevin-wayne/algs4
--- src/main/java/com/example/foundkey/HelloWorld.java
+++ src/main/java/com/example/foundkey/HelloWorld.java
@@ -1,7 +1,9 @@
 package com.example.foundkey;
 
+import edu.princeton.cs.algs4.StdOut;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        StdOut.printf("%s : %d", "Hello World", 42);
     }
 }
