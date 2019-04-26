1e71c8388f69700528204bde0e469512678345e2
foundkey <469456770@qq.com>
2019-04-26 12:27:22 +0800

    add FStack

---
+++ src/main/java/com/example/foundkey/CH01_Fundamentals/Seg1_ProgrammingModel/Sec3/FStack.java
@@ -0,0 +1,131 @@
+package com.example.foundkey.CH01_Fundamentals.Seg1_ProgrammingModel.Sec3;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class FStack<Item> implements Iterable<Item> {
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    private Node mTop;
+    private int mSize;
+
+    public FStack() {
+        mTop = null;
+        mSize = 0;
+
+        assert check();
+    }
+
+    public void push(Item item) {
+        Node oldTop = mTop;
+        mTop = new Node();
+        mTop.item = item;
+        mTop.next = oldTop;
+        mSize++;
+
+        assert check();
+    }
+
+    public Item pop() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Stack underflow");
+        }
+
+        Item item = mTop.item;
+        mTop = mTop.next;
+        mSize--;
+
+        assert check();
+        return item;
+    }
+
+    public Item peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Stack underflow");
+        }
+
+        return mTop.item;
+    }
+
+    public boolean isEmpty() {
+        return mTop == null;
+    }
+
+    public int size() {
+        return mSize;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (Item item : this) {
+            s.append(item).append(" ");
+        }
+
+        return s.toString();
+    }
+
+    private boolean check() {
+        if (mSize < 0) {
+            return false;
+        }
+
+        if (mSize == 0) {
+            if (mTop != null) {
+                return false;
+            }
+        } else if(mSize == 1){
+            if (mTop == null) {
+                return false;
+            }
+            if (mTop.next != null) {
+                return false;
+            }
+        } else {
+            if (mTop == null) {
+                return false;
+            }
+
+            if (mTop.next == null) {
+                return false;
+            }
+        }
+
+        int numberOfNodes = 0;
+        for (Node cur = mTop; cur != null && numberOfNodes <= mSize; cur = cur.next) {
+            numberOfNodes++;
+        }
+
+        return numberOfNodes == mSize;
+    }
+
+    private class FStackIterator implements Iterator<Item> {
+        private Node current = mTop;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Item next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+
+            Item item = current.item;
+            current = current.next;
+
+            return item;
+        }
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new FStackIterator();
+    }
+}
---
+++ src/test/java/com/example/foundkey/CH01_Fundamentals/Seg1_ProgrammingModel/Sec3/FStackTest.java
@@ -0,0 +1,74 @@
+package com.example.foundkey.CH01_Fundamentals.Seg1_ProgrammingModel.Sec3;
+
+import com.sun.org.apache.bcel.internal.generic.FSTORE;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.*;
+
+public class FStackTest {
+    private String[] testData;
+
+    @Before
+    public void setUp() {
+        testData = new String[]{
+                "Apple", "Banana", "Cherry", "Watermelon"
+        };
+    }
+
+    @Test
+    public void useCase() {
+        FStack<String> stack = new FStack<>();
+
+        for (String str : testData) {
+            stack.push(str);
+        }
+
+        Assert.assertEquals(testData.length, stack.size());
+
+        StringBuilder info = new StringBuilder();
+        while (!stack.isEmpty()) {
+            info.append(stack.pop());
+        }
+        Assert.assertEquals("WatermelonCherryBananaApple", info.toString());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void popEmptyStack(){
+        FStack<Integer> stack = new FStack<>();
+
+        stack.pop();
+    }
+
+    @Test
+    public void testIterator() {
+        FStack<String> stack = new FStack<>();
+
+        for (String str : testData) {
+            stack.push(str);
+        }
+
+        StringBuilder info = new StringBuilder();
+        for (String str : stack) {
+            info.append(str);
+        }
+
+        Assert.assertEquals("WatermelonCherryBananaApple", info.toString());
+     }
+
+    @Test
+    public void peek() {
+        FStack<Integer> stack = new FStack<>();
+
+        stack.push(42);
+        stack.push(24);
+        stack.push(34);
+        int item = stack.peek();
+
+        Assert.assertEquals(34, item);
+        Assert.assertEquals(3, stack.size());
+    }
+}
