bf5e1738d2410fa231c3314411ee84fbbd058822
foundkey <469456770@qq.com>
2019-04-26 14:45:39 +0800

    add FQueue

---
+++ src/main/java/com/example/foundkey/CH01_Fundamentals/Seg1_ProgrammingModel/Sec3/FQueue.java
@@ -0,0 +1,154 @@
+package com.example.foundkey.CH01_Fundamentals.Seg1_ProgrammingModel.Sec3;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class FQueue<Item> implements Iterable<Item> {
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    private Node mFirst;
+    private Node mLast;
+    private int mSize;
+
+    public FQueue() {
+        mFirst = null;
+        mLast = null;
+        mSize = 0;
+
+        assert check();
+    }
+
+    public void enqueue(Item item) {
+        Node oldLast = mLast;
+        mLast = new Node();
+        mLast.item = item;
+        mLast.next = null;
+        if (isEmpty()) {
+            mFirst = mLast;
+        } else {
+            oldLast.next = mLast;
+        }
+
+        mSize++;
+
+        assert check();
+    }
+
+    public Item dequeue() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue underflow");
+        }
+
+        Item item = mFirst.item;
+        mFirst = mFirst.next;
+        mSize--;
+
+        if (isEmpty()) {
+            mLast = null;
+        }
+
+        assert check();
+
+        return item;
+    }
+
+    public Item peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue underflow");
+        }
+
+        return mFirst.item;
+    }
+
+    public boolean isEmpty() {
+        return mFirst == null;
+    }
+
+    public int size() {
+        return mSize;
+    }
+
+    private boolean check() {
+        if (mSize < 0) {
+            return false;
+        }
+
+        if (mSize == 0) {
+            if (mFirst != null || mLast != null) {
+                return false;
+            }
+        } else if (mSize == 1) {
+            if (mFirst != mLast) {
+                return false;
+            }
+
+            if (mFirst == null) {
+                return false;
+            }
+
+            if (mFirst.next != null) {
+                return false;
+            }
+        } else {
+            if (mFirst == mLast) {
+                return false;
+            }
+
+            if (mFirst == null || mLast == null) {
+                return false;
+            }
+
+            if (mFirst.next == null || mLast.next != null) {
+                return false;
+            }
+
+            int numberOfNodes = 0;
+            for (Item item : this) {
+                numberOfNodes++;
+            }
+            if (numberOfNodes != mSize) {
+                return false;
+            }
+
+            Node lastNode = mFirst;
+            while (lastNode.next != null) {
+                lastNode = lastNode.next;
+            }
+
+            if (lastNode != mLast) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private class FQueueIterator implements Iterator<Item> {
+        private Node current = mFirst;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Item next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new FQueueIterator();
+    }
+}
---
+++ src/test/java/com/example/foundkey/CH01_Fundamentals/Seg1_ProgrammingModel/Sec3/FQueueTest.java
@@ -0,0 +1,104 @@
+package com.example.foundkey.CH01_Fundamentals.Seg1_ProgrammingModel.Sec3;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FQueueTest {
+
+    private String[] testData;
+
+    @Before
+    public void setUp() {
+        testData = "Apple Banana Cherry Watermelon".split(" ");
+    }
+
+    @Test
+    public void useCase() {
+        FQueue<String> queue = new FQueue<>();
+
+        for (String str : testData) {
+            queue.enqueue(str);
+        }
+
+        Assert.assertEquals(testData.length, queue.size());
+
+        StringBuilder info = new StringBuilder();
+        while (!queue.isEmpty()) {
+            info.append(queue.dequeue());
+        }
+
+        Assert.assertEquals("AppleBananaCherryWatermelon", info.toString());
+    }
+
+    @Test
+    public void iterator() {
+        FQueue<String> queue = new FQueue<>();
+
+        for (String str : testData) {
+            queue.enqueue(str);
+        }
+
+        StringBuilder info = new StringBuilder();
+        for (String str : queue) {
+            info.append(str);
+        }
+
+        Assert.assertEquals("AppleBananaCherryWatermelon", info.toString());
+    }
+
+    @Test
+    public void enqueue() {
+        FQueue<Integer> queue = new FQueue<>();
+
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+
+        assertEquals(3, queue.size());
+        assertFalse(queue.isEmpty());
+    }
+
+    @Test
+    public void dequeue() {
+        FQueue<Integer> queue = new FQueue<>();
+
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+
+        int item = queue.dequeue();
+        assertEquals(1, item);
+        assertEquals(2, queue.size());
+        assertFalse(queue.isEmpty());
+    }
+
+    @Test
+    public void peek() {
+        FQueue<Integer> queue = new FQueue<>();
+
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+
+        int item = queue.peek();
+        assertEquals(1, item);
+        assertEquals(3, queue.size());
+        assertFalse(queue.isEmpty());
+    }
+
+    @Test
+    public void isEmpty() {
+        FQueue<Integer> queue = new FQueue<>();
+
+        assertTrue(queue.isEmpty());
+
+        queue.enqueue(1);
+        assertFalse(queue.isEmpty());
+
+        queue.dequeue();
+        assertTrue(queue.isEmpty());
+    }
+}
