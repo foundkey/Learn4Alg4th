aeeb79c72ff98466fa288c6597654c08457d2370
foundkey <469456770@qq.com>
2019-04-26 09:22:50 +0800

    add Exercise 1.1.24

---
+++ src/main/java/com/example/foundkey/CH01_Fundamentals/Seg1_ProgrammingModel/Exercise_1_1_24.java
@@ -0,0 +1,21 @@
+package com.example.foundkey.CH01_Fundamentals.Seg1_ProgrammingModel;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Exercise_1_1_24 {
+    public static int gcd(int p, int q) {
+        StdOut.printf("p: %d, q: %d\n", p, q);
+        if (q == 0) {
+            return p;
+        }
+
+        int r = p % q;
+        return gcd(q, r);
+    }
+
+    public static void main(String[] args) {
+        int p = 1111111;
+        int q = 1234567;
+        StdOut.printf("gcd(%d, %d) = %d", p, q, gcd(1111111, 1234567));
+    }
+}
