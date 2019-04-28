0035d4bc083397d9f1271bc2a76fd9a0e9cfe3bc
foundkey <469456770@qq.com>
2019-04-28 15:10:16 +0800

    add UF test data

--- src/main/java/com/example/foundkey/HelloWorld.java
+++ src/main/java/com/example/foundkey/HelloWorld.java
@@ -1,5 +1,7 @@
 package com.example.foundkey;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Out;
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.StdOut;
 
@@ -8,5 +10,13 @@ public class HelloWorld {
         StdOut.printf("%s : %d", "Hello World", 42);
         StdOut.main(new String[0]);
         Point2D.main(new String[] {"10", "10", "10"});
+
+        In in = new In("https://algs4.cs.princeton.edu/15uf/tinyUF.txt");
+        StdOut.println(in.readInt());
+        while (!in.isEmpty()) {
+            int p = in.readInt();
+            int q = in.readInt();
+            StdOut.printf("%d %d\n", p, q);
+        }
     }
 }
